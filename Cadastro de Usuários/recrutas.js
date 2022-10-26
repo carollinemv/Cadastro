12bc31c961ece2155c30bbeb1061677eb1b0372f
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-10-26 14:59:03 -0300

    Add files via upload

--- Cadastro de Usuários/recrutas.js
+++ Cadastro de Usuários/recrutas.js
@@ -4,8 +4,8 @@ const campoDeEstudo = prompt("Qual é o campo de estudo do recruta?")
 const anoDeNascimento = prompt("Qual é o ano de nascimento do recruta?")
 
 alert(
-    "Recruta cadastrado com sucesso!" +
-    "Nome completo: " + primeiroNome + " " + sobrenome +
-    "Campo de estudo: " + campoDeEstudo +
-    "Idade: " + (2022 - anoDeNascimento)
+    "Recruta cadastrado com sucesso!\n" +
+    "\nNome completo: " + primeiroNome + " " + sobrenome +
+    "\nCampo de estudo: " + campoDeEstudo +
+    "\nIdade: " + (2022 - anoDeNascimento)
 )
